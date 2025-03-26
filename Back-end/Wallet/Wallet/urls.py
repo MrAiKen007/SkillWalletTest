8145514a1b3f947e0aec0ab5670cbb0fc5a7c4c6
MrAiken007 <jorgepaim2005@gmail.com>
2025-03-26 22:28:20 +0100

    1.6.1v

--- Back-end/Wallet/Wallet/urls.py
+++ Back-end/Wallet/Wallet/urls.py
@@ -1,8 +1,14 @@
+# Wallet/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Bem-vindo à SkillWallet!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contas.urls')),           # URLs da aplicação "contas" na raiz
-    path('investimento/', include('investimento.urls')),  # URLs da aplicação "investimento" com prefixo
+    path('', index, name='index'),  # Página inicial
+    path('contas/', include('contas.urls')),
+    path('investimento/', include('investimento.urls')),
 ]
